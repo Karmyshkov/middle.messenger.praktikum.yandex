f5ebd4d899e06b1f2f5cb8dfa48e4e404a3bcd81
Evgeniy Karmyskov <karmyskove@gmail.com>
2022-06-08 01:05:20 +0700

    added open/close menu methods

--- src/utils/chat.js
+++ src/utils/chat.js
@@ -1,6 +1,8 @@
 const messages = document.querySelectorAll(".list-item")
 const contentDefault = document.querySelector(".chat__column-default")
 const contentDialod = document.querySelector(".chat__column-dialog")
+const btnMenu = document.querySelector(".chat__btn")
+const menu = document.querySelector(".menu")
 
 messages.forEach((message) =>
   message.addEventListener("click", () => addActiveClassName(message))
@@ -16,3 +18,23 @@ function addActiveClassName(message) {
 function removeActiveClassName(message) {
   message.classList.remove("chat_is-active")
 }
+
+function handleOpenMenu() {
+  document.addEventListener("click", closePopupByOutsideZone)
+  this.classList.add("chat__btn_active")
+  menu.classList.add("menu_is-show")
+}
+
+function handleCloseMenu() {
+  document.removeEventListener("click", closePopupByOutsideZone)
+  menu.classList.remove("menu_is-show")
+  btnMenu.classList.remove("chat__btn_active")
+}
+
+function closePopupByOutsideZone(evt) {
+  if (!evt.target.classList.contains("menu_is-show") && evt.target !== btnMenu) {
+    handleCloseMenu()
+  }
+}
+
+btnMenu.addEventListener("click", handleOpenMenu)
