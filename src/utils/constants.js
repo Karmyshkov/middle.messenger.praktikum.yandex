7755d8633d2d55e23b8176c8aba88cc6cccbbe41
Evgeniy Karmyskov <karmyskove@gmail.com>
2022-06-09 01:27:50 +0700

    added values in config

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -4,5 +4,7 @@ const config = {
   inputHelperTextSelector: "input__helper-text",
   isShowHelperTextSelector: "input__helper-text_show",
   btnSubmitFormSelector: "button",
-  isDisableBtnSubmit: "button_disable",
+  isDisableBtnSubmitSelector: "button_disable",
+  labelTextSelector: "input__text",
+  isValuelabelTextSelector: "input__text_isValue",
 }
