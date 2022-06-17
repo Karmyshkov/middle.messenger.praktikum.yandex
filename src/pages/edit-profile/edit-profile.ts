86d931647719920f0ea744987228097f86a148d1
Evgeniy Karmyskov <karmyskove@gmail.com>
2022-06-18 00:18:11 +0700

    added EditProfilePage component

---
+++ src/pages/edit-profile/edit-profile.ts
@@ -0,0 +1,36 @@
+import Block from "core/Block";
+import "./edit-profile.css";
+import dataProfile from "../../data/profile.json";
+
+const { email, login, name, lastName, chatName, phone } = dataProfile.payload;
+
+interface EditProfilePageProps {}
+
+export class EditProfilePage extends Block {
+  constructor(props: EditProfilePageProps) {
+    super({ ...props });
+  }
+  render() {
+    // language=hbs
+    return `
+      <ul class="edit-profile">
+        {{{BtnBackProfile href="/profile"}}}
+        <li class="edit-profile__column">
+          <form class="edit-profile__form">
+            {{{EditAvatar}}}
+            <p class="profile__user-name">Иван</p>
+            <ul class="edit-profile__list">
+              {{{InputProfile type="email" helperText="Почта" value="${email}"}}}
+              {{{InputProfile type="text" helperText="Логин" value="${login}"}}}
+              {{{InputProfile type="text" helperText="Имя" value="${name}"}}}
+              {{{InputProfile type="text" helperText="Фамилия" value="${lastName}"}}}
+              {{{InputProfile type="text" helperText="Имя в чате" value="${chatName}"}}}
+              {{{InputProfile type="phone" helperText="Телефон" value="${phone}"}}}
+              {{{Button typeBtn="submit" textBtn="Сохранить" classes="button_page_edit-profile"}}}
+            </ul>
+          </form>
+        </li>
+      </ul>
+    `;
+  }
+}
