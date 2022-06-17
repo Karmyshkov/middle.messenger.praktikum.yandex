3a3b3ca30019ab293ef8f73fddff984b3c1a2b98
Evgeniy Karmyskov <karmyskove@gmail.com>
2022-06-17 21:50:39 +0700

    added Error component

---
+++ src/components/error/error.ts
@@ -0,0 +1,24 @@
+import Block from "core/Block";
+import "./error.css";
+
+interface ErrorProps {
+  title: string;
+  subtitle: string;
+}
+
+export class Error extends Block {
+  constructor({ title, subtitle }: ErrorProps) {
+    super({ title, subtitle });
+  }
+
+  protected render(): string {
+    // language=hbs
+    return `
+      <div class="error">
+        <h1 class="error__title">{{title}}</h1>
+        <p class="error__subtitle">{{subtitle}}</p>
+        <a class="error__link" href="/">Назад к чатам</a>
+      </div>
+    `;
+  }
+}
