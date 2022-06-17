5bce0ab7912eac04c14bdc89143d6f050e75b6f4
Evgeniy Karmyskov <karmyskove@gmail.com>
2022-06-17 23:27:04 +0700

    added BtnBackProfile component

---
+++ src/components/btnBackProfile/btnBackProfile.ts
@@ -0,0 +1,30 @@
+import Block from "core/Block";
+import "./btnBackProfile.css";
+import left_arrow from "img/left-arrow.svg";
+
+interface BtnBackProfileProps {
+  href: string;
+}
+
+export class BtnBackProfile extends Block {
+  constructor({ href }: BtnBackProfileProps) {
+    super({ href });
+  }
+
+  protected render(): string {
+    // language=hbs
+    return `
+      <li class="profile-btn">
+        <a href="{{href}}" class="profile-btn__link" aria-label="Вернуться назад">
+          <div class="profile-btn__wrap">
+            <img
+              class="profile__icon"
+              src="${left_arrow}"
+              alt="Иконка вернуться назад"
+            />
+          </div>
+        </a>
+      </li>
+    `;
+  }
+}
