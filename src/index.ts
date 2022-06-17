7f034d0c851068cafaa02168e052418ed699ffd4
Evgeniy Karmyskov <karmyskove@gmail.com>
2022-06-17 23:14:51 +0700

    decomposed code in index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,33 +1,11 @@
-import { renderDOM, registerComponent, BlockConstructable } from "core";
-import SigninPage from "pages/signin";
-import SignupPage from "pages/signup";
-import ChatPage from "pages/chat";
-import NotFoundPage from "pages/not-found";
-import ServerErrorPage from "pages/server-error";
-
+import { renderDOM, registerComponent } from "core";
+import { getCurrentPage } from "routes";
 import { components } from "components";
 
 components.forEach((component) => {
   registerComponent(component);
 });
 
-const path = document.location.pathname;
-let app = new SigninPage({});
-
-switch (path) {
-  case "/":
-    app = new SigninPage({});
-    break;
-  case "/signup":
-    app = new SignupPage({});
-    break;
-  case "/chat":
-    app = new ChatPage({});
-    break;
-  default:
-    app = new NotFoundPage({});
-}
-
 document.addEventListener("DOMContentLoaded", () => {
-  renderDOM(app);
+  renderDOM(getCurrentPage());
 });
