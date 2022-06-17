049e632fc5634421417692a7b42621e8a21e7766
Evgeniy Karmyskov <karmyskove@gmail.com>
2022-06-17 18:17:07 +0700

    added routing

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import { renderDOM, registerComponent, BlockConstructable } from "core";
 import SigninPage from "pages/signin";
+import SignupPage from "pages/signup";
 
 import Input from "components/input";
 import Button from "components/button";
@@ -10,6 +11,20 @@ components.forEach((component) => {
   registerComponent(component);
 });
 
+const path = document.location.pathname;
+let app = new SigninPage({});
+
+switch (path) {
+  case "/":
+    app = new SigninPage({});
+    break;
+  case "/signup":
+    app = new SignupPage({});
+    break;
+  default:
+    app = new SigninPage({});
+}
+
 document.addEventListener("DOMContentLoaded", () => {
-  renderDOM(new SigninPage({}));
+  renderDOM(app);
 });
