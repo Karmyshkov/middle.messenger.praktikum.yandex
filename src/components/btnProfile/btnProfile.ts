a02dc949470222ac14965021d09a943b934004d5
Evgeniy Karmyskov <karmyskove@gmail.com>
2022-06-18 00:07:24 +0700

    added BtnProfile component

---
+++ src/components/btnProfile/btnProfile.ts
@@ -0,0 +1,25 @@
+import Block from "core/Block";
+import "./btnProfile.css";
+
+interface BtnProfileProps {
+  text: string;
+  classes: string;
+  href: string;
+}
+
+export class BtnProfile extends Block {
+  constructor({ text, classes, href }: BtnProfileProps) {
+    super({ text, classes, href });
+  }
+
+  protected render(): string {
+    // language=hbs
+    return `
+      <li class="btn-profile">
+        <a class="btn-profile__link {{classes}}" href={{href}}>
+          {{text}}
+        </a>
+      </li>
+    `;
+  }
+}
