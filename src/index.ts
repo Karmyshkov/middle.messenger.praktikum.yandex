5c61f816b29cb6033f9d2fb9e44b4898d1198ac4
Evgeniy Karmyskov <karmyskove@gmail.com>
2022-06-17 21:52:15 +0700

    added routes for created pages

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,15 @@
 import { renderDOM, registerComponent, BlockConstructable } from "core";
 import SigninPage from "pages/signin";
 import SignupPage from "pages/signup";
+import ChatPage from "pages/chat";
+import NotFoundPage from "pages/not-found";
+import ServerErrorPage from "pages/server-error";
 
 import Input from "components/input";
 import Button from "components/button";
+import Error from "components/error";
 
-const components: BlockConstructable<any>[] = [Input, Button];
+const components: BlockConstructable<any>[] = [Input, Button, Error];
 
 components.forEach((component) => {
   registerComponent(component);
@@ -21,8 +25,11 @@ switch (path) {
   case "/signup":
     app = new SignupPage({});
     break;
+  case "/chat":
+    app = new ChatPage({});
+    break;
   default:
-    app = new SigninPage({});
+    app = new NotFoundPage({});
 }
 
 document.addEventListener("DOMContentLoaded", () => {
