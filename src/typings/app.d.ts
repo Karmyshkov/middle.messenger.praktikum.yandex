baa6114c1d0aa2a3b615a5dc31ca774838fcd879
Evgeniy Karmyskov <karmyskove@gmail.com>
2022-06-17 18:46:56 +0700

    added types

--- src/typings/app.d.ts
+++ src/typings/app.d.ts
@@ -1,8 +1,13 @@
 declare global {
-  export type Nullable<T> = T | null
+  export type Nullable<T> = T | null;
 
-  export type Keys<T extends Record<string, unknown>> = keyof T
-  export type Values<T extends Record<string, unknown>> = T[Keys<T>]
+  export type Keys<T extends Record<string, unknown>> = keyof T;
+  export type Values<T extends Record<string, unknown>> = T[Keys<T>];
+
+  export module "*.svg";
+  export module "*.jpg";
+  export module "*.png";
+  export module "*.webp";
 }
 
-export {}
+export {};
