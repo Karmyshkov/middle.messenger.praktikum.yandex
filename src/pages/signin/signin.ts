9105f72634b231087cae81ba061e57cde402c853
Evgeniy Karmyskov <karmyskove@gmail.com>
2022-06-17 18:21:38 +0700

    refactoring signin page

--- src/pages/signin/signin.ts
+++ src/pages/signin/signin.ts
@@ -17,7 +17,7 @@ export class SigninPage extends Block {
             {{{Input type="text" helperText="Логин" minlength="5" maxlength="20"}}}
             {{{Input type="password" helperText="Пароль" minlength="4" maxlength="20" classes="input_is-auth"}}}
             {{{Button href="./chat.hbs" textBtn="Авторизоваться"}}}
-            <a class="signin__link" href="./register.hbs">Нет аккаунта?</a>
+            <a class="signin__link" href="/signup">Нет аккаунта?</a>
           </form>
         </main>
       </div>
