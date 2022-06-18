b46dd61346927b7375ffdc6a34ba86bb22c1614a
Evgeniy Karmyskov <karmyskove@gmail.com>
2022-06-19 00:06:47 +0700

    added ListItem component

---
+++ src/components/listItem/listItem.ts
@@ -0,0 +1,37 @@
+import Block from "core/Block";
+import "./listItem.css";
+import { ChatType } from "types";
+
+export class ListItem extends Block {
+  constructor({ userName, lastMessage, time, countNotReadMessage, srcAvatar }: ChatType) {
+    super({ userName, lastMessage, time, countNotReadMessage, srcAvatar });
+  }
+  protected getStateFromProps(props: ChatType): void {
+    this.state = {
+      userName: props.userName,
+      lastMessage: props.lastMessage,
+      time: props.time,
+      countNotReadMessage: props.countNotReadMessage,
+      srcAvatar: props.srcAvatar,
+    };
+  }
+  protected render(): string {
+    const { userName, lastMessage, time, countNotReadMessage, srcAvatar } = this.state;
+    // language=hbs
+    return `
+      <li class="list-item">
+        <div class="list-item__container">
+          {{{Avatar srcAvatar="${srcAvatar}" userName="${userName}"}}}
+          <div class="list-item__inner">
+            <p class="list-item__user-name">${userName}</p>
+            <p class="list-item__message">{{#unless ${countNotReadMessage}}}<span class="list-item__message list-item__message_bold">Вы:</span>{{/unless}}${lastMessage}</p>
+          </div>
+          <div class="list-item__wrap">
+            <time class="list-item__time">${time}</time>
+            <p class="list-item__count-message {{#if ${countNotReadMessage}}}list-item__count-message_is-show{{/if}}">${countNotReadMessage}</p>
+          </div>
+        </div>
+      </li>
+    `;
+  }
+}
