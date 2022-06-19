e1bef9b2a2aa00bf024a8ef31f15a8d45fb91df9
Evgeniy Karmyskov <karmyskove@gmail.com>
2022-06-19 16:03:15 +0700

    refactoring code

--- src/utils/signin.js
+++
@@ -1,5 +0,0 @@
-const signinFormValidator = new FormValidator(inputValidateConfig, "signin")
-signinFormValidator.enableValidation()
-
-const signinInput = new Input(inputValidateConfig, "signin")
-signinInput.checkOnValueInput()
