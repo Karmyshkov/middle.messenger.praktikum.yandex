0d26b758288225a4909b501271378385e8db56cd
Evgeniy Karmyskov <karmyskove@gmail.com>
2022-06-19 22:14:56 +0700

    added View abstract class

---
+++ src/utils/View.ts
@@ -0,0 +1,14 @@
+export abstract class View {
+  protected _messagesSelector!: string;
+  protected _contentDefaultSelector!: string;
+  protected _contentDialodSelector!: string;
+  protected _isActiveChatSelector!: string;
+  protected _hiddenChatSelecor!: string;
+  protected _searchInputByChatsSelector!: string;
+  protected _imgFromSearchInputByChatsSelector!: string;
+  protected _messages!: NodeList;
+  protected _contentDefault!: Nullable<HTMLElement>;
+  protected _contentDialod!: Nullable<HTMLElement>;
+  protected _searchInputByChats!: Nullable<HTMLElement>;
+  protected _imgFromSearchInputByChats!: Nullable<HTMLElement>;
+}
