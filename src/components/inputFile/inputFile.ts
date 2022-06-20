fee83f47045792ea2017cb4e8c4fe94b804490be
Evgeniy Karmyskov <karmyskove@gmail.com>
2022-06-20 16:26:18 +0700

    added InputFile component

---
+++ src/components/inputFile/inputFile.ts
@@ -0,0 +1,14 @@
+import Block from 'core/Block';
+import './inputFile.css';
+
+export class InputFile extends Block {
+  protected render(): string {
+    // language=hbs
+    return `
+      <label class="input-file">
+        <input class="input-file__input" type="file" />
+        <span class="input-file__span">Выбрать файл на компьютере</span>
+      </label>
+    `;
+  }
+}
