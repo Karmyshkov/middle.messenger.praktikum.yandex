563f60911966bfc882ec82bae30fa78647b51936
Evgeniy Karmyskov <karmyskove@gmail.com>
2022-06-20 21:20:05 +0700

    added FormValidation class

---
+++ src/utils/FormValidator.ts
@@ -0,0 +1,51 @@
+import { View } from './View';
+
+export class FormValidator extends View {
+  constructor(config: Record<string, string>, formSelector: string) {
+    super();
+    this._formSelector = formSelector;
+    this._inputSelector = config.inputSelector;
+    this._inputHelperTextSelector = config.inputHelperTextSelector;
+    this._btnSubmitFormSelector = config.btnSubmitFormSelector;
+    this._isShowHelperTextSelector = config.isShowHelperTextSelector;
+    this._isDisableBtnSubmitSelector = config.isDisableBtnSubmitSelector;
+    this._form = document.querySelector(`.${this._formSelector}`);
+    this._inputs = this._form
+      ? Array.from(this._form.querySelectorAll(`.${this._inputSelector}`))
+      : null;
+    this._btnSubmit = this._form
+      ? this._form.querySelector(`.${this._btnSubmitFormSelector}`)
+      : null;
+    this._errorContainer = null;
+  }
+
+  private _closeErrorMessage() {
+    if (this._errorContainer) {
+      this._errorContainer.textContent = '';
+      this._errorContainer.classList.remove(this._isShowHelperTextSelector);
+    }
+  }
+
+  private _showErrorMessage(validationMessage: string) {
+    if (this._errorContainer) {
+      this._errorContainer.textContent = validationMessage;
+      this._errorContainer.classList.add(this._isShowHelperTextSelector);
+    }
+  }
+
+  public handleFieldValidation(evt: Event) {
+    const element = evt.target;
+
+    this._errorContainer = (<Element>(
+      element
+    ))?.parentElement?.parentElement?.querySelector(`.${this._inputHelperTextSelector}`);
+
+    !(<HTMLFormElement>element).validity.valid
+      ? this._showErrorMessage((<HTMLFormElement>element).validationMessage)
+      : this._closeErrorMessage();
+  }
+
+  public clearError() {
+    this._closeErrorMessage();
+  }
+}
