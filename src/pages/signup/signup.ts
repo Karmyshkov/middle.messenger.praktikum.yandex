28fe0952ce2ce3f4ea0209685d962e0f9dfc0dab
Evgeniy Karmyskov <karmyskove@gmail.com>
2022-06-21 15:49:03 +0700

    added submit function

--- src/pages/signup/signup.ts
+++ src/pages/signup/signup.ts
@@ -1,6 +1,7 @@
 import Block from 'core/Block';
 import './signup.css';
 import { Input } from 'utils/Input';
+import { FormValidator } from 'utils/FormValidator';
 import { config } from 'utils/constants';
 
 export class SignupPage extends Block {
@@ -9,6 +10,20 @@ export class SignupPage extends Block {
       handleChangeInput: (evt: Event) => {
         evt.target && new Input(config, evt.target).checkOnValueInput();
       },
+      hendleSubmitForm: (evt: Event) => {
+        evt.preventDefault();
+
+        if (FormValidator.checkStateForm('signup')) {
+          const inputs = this.element?.querySelectorAll('.input__text-field');
+          let dataForm = {};
+
+          inputs?.forEach((input) => {
+            const inputElement = input as HTMLInputElement;
+            dataForm = { ...dataForm, [inputElement.name]: inputElement.value };
+          });
+          console.log(dataForm);
+        }
+      },
     };
   }
   render() {
@@ -16,16 +31,16 @@ export class SignupPage extends Block {
     return `
       <div class="page">
         <main class="page__form">
-          <form class="signup">
+          <form class="signup" name="signup">
             <h1 class="signup__title">Регистрация</h1>
-            {{{InputWrapper onInput=handleChangeInput type="email" helperText="Почта"}}}
-            {{{InputWrapper onInput=handleChangeInput type="text" helperText="Логин" minlength="3" maxlength="20"}}}
-            {{{InputWrapper onInput=handleChangeInput type="text" helperText="Имя" minlength="1" maxlength="50"}}}
-            {{{InputWrapper onInput=handleChangeInput type="text" helperText="Фамилия" minlength="1" maxlength="50"}}}
-            {{{InputWrapper onInput=handleChangeInput type="text" helperText="Телефон" minlength="10" maxlength="15"}}}
-            {{{InputWrapper onInput=handleChangeInput type="password" helperText="Пароль" minlength="8" maxlength="40"}}}
-            {{{InputWrapper onInput=handleChangeInput type="password" helperText="Пароль (ещё раз)" minlength="8" maxlength="40" classes="input_is-auth"}}}
-            {{{Button textBtn="Зарегистрироваться" type=""submit}}}
+            {{{InputWrapper onInput=handleChangeInput type="email" helperText="Почта" name="email"}}}
+            {{{InputWrapper onInput=handleChangeInput type="text" helperText="Логин" minlength="3" maxlength="20" name="login"}}}
+            {{{InputWrapper onInput=handleChangeInput type="text" helperText="Имя" minlength="1" maxlength="50" name="name"}}}
+            {{{InputWrapper onInput=handleChangeInput type="text" helperText="Фамилия" minlength="1" maxlength="50" name="lastName"}}}
+            {{{InputWrapper onInput=handleChangeInput type="text" helperText="Телефон" minlength="10" maxlength="15" name="phone"}}}
+            {{{InputWrapper onInput=handleChangeInput type="password" helperText="Пароль" minlength="8" maxlength="40" name="password"}}}
+            {{{InputWrapper onInput=handleChangeInput type="password" helperText="Пароль (ещё раз)" minlength="8" maxlength="40" classes="input_is-auth" name="repeatPassword"}}}
+            {{{Button onClick=hendleSubmitForm textBtn="Зарегистрироваться" type="submit"}}}
             <a class="signup__link" href="/">Войти</a>
           </form>
         </main>
