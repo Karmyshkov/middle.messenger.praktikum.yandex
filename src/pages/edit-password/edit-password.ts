edd046d314a83782b9cd406b2093ba520e722b4a
Evgeniy Karmyskov <karmyskove@gmail.com>
2022-06-21 15:57:29 +0700

    refactiring

--- src/pages/edit-password/edit-password.ts
+++ src/pages/edit-password/edit-password.ts
@@ -1,26 +1,43 @@
 import Block from 'core/Block';
 import './edit-password.css';
+import { Popup } from 'utils/Popup';
+import { config } from 'utils/constants';
 import dataProfile from 'data/profile.json';
 
 export class EditPasswordPage extends Block {
+  protected getStateFromProps() {
+    this.state = {
+      handleEditAvatar: () => {
+        new Popup(
+          'popup_change-avatar',
+          'edit-avatar',
+          'popup_opened',
+          config
+        ).handleOpenPopup();
+      },
+    };
+  }
   render() {
     // language=hbs
     return `
-      <ul class="edit-profile">
-        {{{BtnBackProfile href="/profile"}}}
-        <li class="edit-profile__column">
-          <form class="edit-profile__form">
-            {{{EditAvatar}}}
-            <p class="profile__user-name">Иван</p>
-            <ul class="edit-profile__list">
-              {{{InputProfile type="password" helperText="Старый пароль" value="${dataProfile.payload.password}"}}}
-              {{{InputProfile type="password" helperText="Новый пароль" value="1234"}}}
-              {{{InputProfile type="password" helperText="Повторите новый пароль" value="1234"}}}
-              {{{Button textBtn="Сохранить" classes="button_page_edit-password" type="submit"}}}
-            </ul>
-          </form>
-        </li>
-      </ul>
+      <div class="profile">
+        <ul class="profile__wrapper">
+          {{{BtnBackProfile href="/profile"}}}
+          <li class="edit-profile__column">
+            <form class="edit-profile__form">
+              {{{EditAvatar onClick=handleEditAvatar}}}
+              <p class="profile__user-name">Иван</p>
+              <ul class="edit-profile__list">
+                {{{InputProfile type="password" helperText="Старый пароль" value="${dataProfile.payload.password}"}}}
+                {{{InputProfile type="password" helperText="Новый пароль" value="1234"}}}
+                {{{InputProfile type="password" helperText="Повторите новый пароль" value="1234"}}}
+                {{{Button textBtn="Сохранить" classes="button_page_edit-password" type="submit"}}}
+              </ul>
+            </form>
+          </li>
+        </ul>
+        {{{Popup title="Загрузите файл" textBtn="Поменять" classesPopup="popup_change-avatar" isDefault=false}}}
+      </div>
     `;
   }
 }
--- src/pages/edit-profile/edit-profile.ts
+++ src/pages/edit-profile/edit-profile.ts
@@ -1,31 +1,48 @@
 import Block from 'core/Block';
 import './edit-profile.css';
+import { Popup } from 'utils/Popup';
+import { config } from 'utils/constants';
 import dataProfile from 'data/profile.json';
 
 const { email, login, name, lastName, chatName, phone } = dataProfile.payload;
 
 export class EditProfilePage extends Block {
+  protected getStateFromProps() {
+    this.state = {
+      handleEditAvatar: () => {
+        new Popup(
+          'popup_change-avatar',
+          'edit-avatar',
+          'popup_opened',
+          config
+        ).handleOpenPopup();
+      },
+    };
+  }
   render() {
     // language=hbs
     return `
-      <ul class="edit-profile">
-        {{{BtnBackProfile href="/profile"}}}
-        <li class="edit-profile__column">
-          <form class="edit-profile__form">
-            {{{EditAvatar}}}
-            <p class="profile__user-name">Иван</p>
-            <ul class="edit-profile__list">
-              {{{InputProfile type="email" helperText="Почта" value="${email}"}}}
-              {{{InputProfile type="text" helperText="Логин" value="${login}"}}}
-              {{{InputProfile type="text" helperText="Имя" value="${name}"}}}
-              {{{InputProfile type="text" helperText="Фамилия" value="${lastName}"}}}
-              {{{InputProfile type="text" helperText="Имя в чате" value="${chatName}"}}}
-              {{{InputProfile type="phone" helperText="Телефон" value="${phone}"}}}
-              {{{Button textBtn="Сохранить" classes="button_page_edit-profile" type="submit"}}}
-            </ul>
-          </form>
-        </li>
-      </ul>
+      <div class="profile">
+        <ul class="profile__wrapper">
+          {{{BtnBackProfile href="/profile"}}}
+          <li class="edit-profile__column">
+            <form class="edit-profile__form">
+              {{{EditAvatar onClick=handleEditAvatar}}}
+              <p class="profile__user-name">Иван</p>
+              <ul class="edit-profile__list">
+                {{{InputProfile type="email" helperText="Почта" value="${email}"}}}
+                {{{InputProfile type="text" helperText="Логин" value="${login}"}}}
+                {{{InputProfile type="text" helperText="Имя" value="${name}"}}}
+                {{{InputProfile type="text" helperText="Фамилия" value="${lastName}"}}}
+                {{{InputProfile type="text" helperText="Имя в чате" value="${chatName}"}}}
+                {{{InputProfile type="phone" helperText="Телефон" value="${phone}"}}}
+                {{{Button textBtn="Сохранить" classes="button_page_edit-profile" type="submit"}}}
+              </ul>
+            </form>
+          </li>
+        </ul>
+        {{{Popup title="Загрузите файл" textBtn="Поменять" classesPopup="popup_change-avatar" isDefault=false}}}
+      </div>
     `;
   }
 }
