76ff006b3bbebc69966102f784785e85470b7fb1
Evgeniy Karmyskov <karmyskove@gmail.com>
2022-06-21 15:40:29 +0700

    added field for PopupProps

--- src/components/popup/types.ts
+++ src/components/popup/types.ts
@@ -2,6 +2,7 @@ export interface PopupProps {
   onClick: () => void;
   classesPopup?: string;
   classesForm?: string;
+  name: string;
   title: string;
   isDefault: boolean;
   helperText?: Text;
