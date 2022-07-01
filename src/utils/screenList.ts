38ac4986b7f2701df71679ec1e290594b783080b
Evgeniy Karmyskov <karmyskove@gmail.com>
2022-07-01 19:02:25 +0700

    added params for another pages

--- src/utils/screenList.ts
+++ src/utils/screenList.ts
@@ -2,12 +2,18 @@ import { BlockClass } from 'types';
 import { Screens } from 'types';
 import Signin from 'pages/signin';
 import Signup from 'pages/signup';
+import Messenger from 'pages/chat';
 import Profile from 'pages/profile';
+import EditProfile from 'pages/edit-profile';
+import EditPassword from 'pages/edit-password';
 
 const map: Record<Screens, BlockClass<any>> = {
   [Screens.Signin]: Signin,
   [Screens.Signup]: Signup,
+  [Screens.Messenger]: Messenger,
   [Screens.Profile]: Profile,
+  [Screens.EditProfle]: EditProfile,
+  [Screens.EditPassword]: EditPassword,
 };
 
 const getScreenComponent = (screen: Screens): BlockClass<any> => {
