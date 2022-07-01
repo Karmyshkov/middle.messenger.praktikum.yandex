b91bbd422ad0921fa0e65d1f770a78ffbdbef8a4
Evgeniy Karmyskov <karmyskove@gmail.com>
2022-07-01 18:03:43 +0700

    added Route class

---
+++ src/core/Route.ts
@@ -0,0 +1,50 @@
+import { BlockClass, props } from 'types';
+import renderDOM from './renderDOM';
+import Block from 'core/Block';
+import { isEqual } from 'utils';
+
+export class Route<P = any> {
+  private pathname: string;
+  private blockClass: BlockClass<P>;
+  private block: Block | null = null;
+  private props: props;
+  private isPrefixId: boolean | undefined;
+
+  constructor(pathname: string, view: BlockClass<P>, props: props) {
+    this.isPrefixId = pathname.includes(':id');
+    this.pathname = pathname.replace('/:id', '');
+    this.blockClass = view;
+    this.props = props;
+  }
+  navigate(pathname: string) {
+    if (this.match(pathname)) {
+      this.render();
+    }
+  }
+  leave() {
+    if (this.block) {
+      this.block.hide();
+    }
+  }
+  match(pathname: string) {
+    if (this.isPrefixId) {
+      pathname = pathname.replace(/\/\d+/, '');
+    }
+    return isEqual(pathname, this.pathname);
+  }
+  private prefixHandler() {
+    const id = Number(window.location.pathname.replace(/[a-zA-Z/]+/, ''));
+    return { id };
+  }
+  render() {
+    const { id } = this.prefixHandler();
+    if (!this.block) {
+      this.block = new this.blockClass({ ...this.props, idPath: id });
+      renderDOM(this.block);
+      return;
+    }
+
+    this.block.setProps({ idPath: id });
+    this.block.show();
+  }
+}
