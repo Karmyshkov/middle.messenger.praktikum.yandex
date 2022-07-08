18e475c27fd888bd213dfe962ad427adbe87a57d
Evgeniy Karmyskov <karmyskove@gmail.com>
2022-07-08 22:24:53 +0700

    refactoring BaseAPI class

--- src/Api/BaseAPI.ts
+++ src/Api/BaseAPI.ts
@@ -1,15 +1,25 @@
+import { HTTPTransport } from 'utils/classes/HTTPTransport';
 import { NOT_IMPLEMENTED_ERROR } from 'utils/constants';
 
-export class BaseAPI {
-  create() {
+export abstract class BaseAPI {
+  _http: HTTPTransport;
+  _baseUrl: string;
+  _headers: Record<string, string>;
+  constructor({ path }: Record<string, string>) {
+    this._http = new HTTPTransport();
+    this._baseUrl = `https://ya-praktikum.tech/api/v2${path}`;
+    this._headers = { 'Content-Type': 'application/json' };
+  }
+
+  post(url: string, data: any) {
+    return this._http.post(`${this._baseUrl}/${url}`, { data, headers: this._headers });
+  }
+
+  get() {
     throw new Error(NOT_IMPLEMENTED_ERROR);
   }
 
-  request() {
-    throw new Error(NOT_IMPLEMENTED_ERROR);
-  }
-
-  update() {
+  put() {
     throw new Error(NOT_IMPLEMENTED_ERROR);
   }
 
