7ce2a86a23f3535de512e91798722ea552443e25
Evgeniy Karmyskov <karmyskove@gmail.com>
2022-07-08 18:26:22 +0700

    added BaseAPI class

---
+++ src/Api/BaseAPI.ts
@@ -0,0 +1,17 @@
+export class BaseAPI {
+  create() {
+    throw new Error('Not implemented');
+  }
+
+  request() {
+    throw new Error('Not implemented');
+  }
+
+  update() {
+    throw new Error('Not implemented');
+  }
+
+  delete() {
+    throw new Error('Not implemented');
+  }
+}
