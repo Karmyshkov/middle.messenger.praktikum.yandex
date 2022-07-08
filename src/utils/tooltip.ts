f0fc398513d6baab0d4735b1abe24d023fb03d6c
Evgeniy Karmyskov <karmyskove@gmail.com>
2022-07-09 00:46:21 +0700

    added tooltip

---
+++ src/utils/tooltip.ts
@@ -0,0 +1,17 @@
+type TooltipProp = {
+  text: string;
+  type: 'error' | 'success';
+};
+
+const tooltip = document.createElement('div');
+tooltip.classList.add('tooltip');
+
+function showTooltip({ text, type }: TooltipProp) {
+  tooltip.textContent = text;
+  tooltip.classList.add(type === 'success' ? 'tooltip_is-success' : 'tooltip_is-error');
+  document.body.append(tooltip);
+
+  setTimeout(() => document.body.removeChild(tooltip), 5000);
+}
+
+export { showTooltip };
