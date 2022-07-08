03d056a295566d774dc9803376df62607540d070
Evgeniy Karmyskov <karmyskove@gmail.com>
2022-07-09 01:29:57 +0700

    added close tooltip by click

--- src/utils/tooltip.ts
+++ src/utils/tooltip.ts
@@ -11,12 +11,18 @@ function showTooltip({ text, type }: TooltipProp) {
   tooltip.classList.add(type === 'success' ? 'tooltip_is-success' : 'tooltip_is-error');
   document.body.append(tooltip);
 
+  tooltip.addEventListener('click', handleCloseTooltip);
+
   setTimeout(() => {
     tooltip.classList.remove(
       type === 'success' ? 'tooltip_is-success' : 'tooltip_is-error'
     );
     document.body.contains(tooltip) && document.body.removeChild(tooltip);
   }, 5000);
+}
+
+function handleCloseTooltip() {
+  document.body.removeChild(tooltip);
 }
 
 export { showTooltip };
