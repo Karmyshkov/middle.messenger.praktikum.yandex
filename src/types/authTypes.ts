6eb40d0a8bc33d9a1d63cab61bcab57187e14d09
Evgeniy Karmyskov <karmyskove@gmail.com>
2022-07-08 23:12:06 +0700

    decomposed code

---
+++ src/types/authTypes.ts
@@ -0,0 +1,10 @@
+interface SignupType {
+  email: string;
+  login: string;
+  first_name: string;
+  second_name: string;
+  phone: string;
+  password: string;
+}
+
+export { SignupType };
--- src/types/types.ts
+++ src/types/types.ts
@@ -45,22 +45,4 @@ interface MessageProps {
 
 type props = Record<string, any>;
 
-interface SignupType {
-  email: string;
-  login: string;
-  first_name: string;
-  second_name: string;
-  phone: string;
-  password: string;
-}
-
-export {
-  BlockClass,
-  InputType,
-  ButtonType,
-  Screens,
-  ChatType,
-  MessageProps,
-  props,
-  SignupType,
-};
+export { BlockClass, InputType, ButtonType, Screens, ChatType, MessageProps, props };
