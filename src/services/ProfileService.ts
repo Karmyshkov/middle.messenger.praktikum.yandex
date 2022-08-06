5dc5dd185786fca866e3683acfd4e3c936d2e60d
Evgeniy Karmyskov <karmyskove@gmail.com>
2022-08-06 19:59:21 +0700

    added ProfileService

---
+++ src/services/ProfileService.ts
@@ -0,0 +1,9 @@
+import { chatApi } from 'api';
+import { CreateChatType } from 'types';
+import { showTooltip, showError } from 'utils';
+import { SUCCESS_CREATE_MESSAGE } from 'utils/constants';
+import store from 'core/Store';
+
+class ProfileService {}
+
+export default new ProfileService();
