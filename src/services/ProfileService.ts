de01fe40fecdc4dab42806c54c8b7ceb187e820b
Evgeniy Karmyskov <karmyskove@gmail.com>
2022-08-07 19:46:01 +0700

    added changeUserPassword in ProfileService

--- src/services/ProfileService.ts
+++ src/services/ProfileService.ts
@@ -5,7 +5,7 @@ import {
   SUCCESS_CHANGE_USER_INFO_MESSAGE,
 } from 'utils/constants';
 import store from 'core/Store';
-import { UserInfoDTO } from 'types';
+import { UserInfoDTO, UserPasswordDTO } from 'types';
 
 class ProfileService {
   public changeAvatar(avatar: FormData) {
@@ -30,6 +30,17 @@ class ProfileService {
           type: 'success',
         });
       })
+      .catch(showError);
+  }
+  public changeUserPassword(userPassword: UserPasswordDTO) {
+    profileApi
+      .changeUserPassword(userPassword)
+      .then(() =>
+        showTooltip({
+          text: SUCCESS_CHANGE_USER_INFO_MESSAGE,
+          type: 'success',
+        })
+      )
       .catch(showError);
   }
 }
