dc30c8c4c94795a6a90638ae5384fe8a36b83956
Evgeniy Karmyskov <karmyskove@gmail.com>
2022-08-07 15:23:05 +0700

    changes BtnBackProfile component

--- src/components/btnBackProfile/btnBackProfile.ts
+++ src/components/btnBackProfile/btnBackProfile.ts
@@ -5,19 +5,15 @@ import left_arrow from 'img/left-arrow.svg';
 
 export class BtnBackProfile extends Block {
   static componentName = 'BtnBackProfile';
-  constructor({ href }: BtnBackProfileProps) {
-    super({ href });
+  constructor({ onClick }: BtnBackProfileProps) {
+    super({ events: { click: onClick } });
   }
-  protected getStateFromProps(props: BtnBackProfileProps): void {
-    this.state = {
-      href: props.href,
-    };
-  }
+
   protected render(): string {
     // language=hbs
     return `
       <li class="profile-btn">
-        <a href="${this.state.href}" class="profile-btn__link" aria-label="Вернуться назад">
+        <Button class="profile-btn__link" aria-label="Вернуться назад">
           <div class="profile-btn__wrap">
             <img
               class="profile__icon"
@@ -25,7 +21,7 @@ export class BtnBackProfile extends Block {
               alt="Иконка вернуться назад"
             />
           </div>
-        </a>
+        </Button>
       </li>
     `;
   }
--- src/components/btnBackProfile/types.ts
+++ src/components/btnBackProfile/types.ts
@@ -1,3 +1,3 @@
 export interface BtnBackProfileProps {
-  href: string;
+  onClick: () => void;
 }
