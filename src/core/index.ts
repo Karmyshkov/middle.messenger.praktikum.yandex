3f51055c4f8670cd44104c240df150bb026bc594
Evgeniy Karmyskov <karmyskove@gmail.com>
2022-08-14 16:04:57 +0700

    clear code

--- src/core/index.ts
+++ src/core/index.ts
@@ -2,4 +2,4 @@ export { default as Block } from './Block';
 export { default as registerComponent, BlockConstructable } from './registerComponent';
 export { default as renderDOM } from './renderDOM';
 export { default as BrowseRouter } from './BrowseRouter';
-export { default as store, StoreEvents } from './Store';
+export { default as store } from './Store';
