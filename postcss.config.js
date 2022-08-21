e57421376de6fc1a00ffa8961cf60e242ce7aa10
Evgeniy Karmyskov <karmyskove@gmail.com>
2022-08-22 05:29:46 +0700

    added postcss config

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,7 @@
 const autoprefixer = require('autoprefixer');
 const cssnano = require('cssnano');
+const postcssNested = require('postcss-nested');
 
 module.exports = {
-  plugins: [autoprefixer, cssnano({ preset: 'default' })],
+  plugins: [autoprefixer, postcssNested, cssnano({ preset: 'default' })],
 };
