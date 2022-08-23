f0685cd8d4693a1c7f8a892b74833da1307f7fba
Evgeniy Karmyskov <karmyskove@gmail.com>
2022-08-23 15:31:33 +0700

    added unit test for check is not null init store

--- src/core/test/Store.test.ts
+++ src/core/test/Store.test.ts
@@ -2,6 +2,10 @@ import { expect } from 'chai';
 import store from '../Store';
 
 describe('core/Store', () => {
+  it('Initial store not should be empty', () => {
+    expect(store.getState()).to.be.not.null;
+  });
+
   it('should set state', () => {
     store.setState({ users: 'test' });
 
