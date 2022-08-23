3aa45c6075e330bb077ab46e5fab5d0e90659317
Evgeniy Karmyskov <karmyskove@gmail.com>
2022-08-23 17:40:21 +0700

    clear code

--- test/babel-register.js
+++
@@ -1,8 +0,0 @@
-const { JSDOM } = require('jsdom');
-const register = require('@babel/register').default;
-
-register({ extensions: ['.ts', '.js'] });
-
-const dom = new JSDOM('<div class="app"><div>', { url: 'http://localhost' });
-global.window = dom.window;
-global.document = dom.window.document;
