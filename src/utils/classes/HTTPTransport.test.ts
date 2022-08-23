707549a0c7a782f9cd8d32a54ba2c109dd1e6f32
Evgeniy Karmyskov <karmyskove@gmail.com>
2022-08-23 20:26:48 +0700

    added unit tests

--- src/utils/classes/HTTPTransport.test.ts
+++ src/utils/classes/HTTPTransport.test.ts
@@ -5,7 +5,14 @@ describe('utils/HTTPTransport', () => {
   const http = new HTTPTransport();
 
   describe('auth', () => {
-    it('should return OK', async () => {
+    it('should return error by return user info', async () => {
+      await http.get(`${URLS.BASE}/auth/user`, {}).catch((err) => {
+        const textError = JSON.parse(err.responseText).reason;
+        expect(textError).toEqual('Cookie is not valid');
+      });
+    });
+
+    it('should return OK by login', async () => {
       await http
         .post(`${URLS.BASE}/auth/signin`, {
           headers: HEADERS.CT_APPLICATION_JSON,
@@ -14,7 +21,7 @@ describe('utils/HTTPTransport', () => {
         .then((data: any) => expect(data.responseText).toEqual('OK'));
     });
 
-    it('should return error', async () => {
+    it('should return error by login', async () => {
       await http
         .post(`${URLS.BASE}/auth/signin`, {
           headers: HEADERS.CT_APPLICATION_JSON,
@@ -24,6 +31,24 @@ describe('utils/HTTPTransport', () => {
           const textError = JSON.parse(err.responseText).reason;
           expect(textError).toEqual('User already in system');
         });
+    });
+
+    it('should return user info', async () => {
+      await http.get(`${URLS.BASE}/auth/user`, {}).then((userInfo: any) => {
+        const user = {
+          id: 52802,
+          first_name: 'Sasda124sd',
+          second_name: 'asdasdASDasdasdASD',
+          display_name: 'asdasdasdasd1',
+          login: 'as1211516dasd',
+          avatar:
+            '/78cf0d70-5ddf-448d-a98d-e3dbca94cead/deb492ce-90c2-45ed-a33f-aacafaf24715_scale_1200.webp',
+          email: 'qwmasde@gmail.com',
+          phone: '+79609516683',
+        };
+
+        expect(JSON.parse(userInfo.responseText)).toMatchObject(user);
+      });
     });
   });
 });
