43784bcd5f3d8c098ba45b9b50b2e1811b48cb2c
Evgeniy Karmyskov <karmyskove@gmail.com>
2022-08-23 17:57:01 +0700

    refactoring code

--- src/core/test/EventBus.test.ts
+++ src/core/test/EventBus.test.ts
@@ -1,29 +1,14 @@
-import { expect } from 'chai';
 import EventBus from '../EventBus';
 
 describe('core/EventBus', () => {
   const eventBus = new EventBus();
-  let text = '' as string | null;
 
-  function mock() {
-    text = 'mock';
-  }
-
-  beforeEach(() => {
-    text = null;
-  });
+  const mock = jest.fn();
 
   it('on and emit event', () => {
     eventBus.on('test-event', mock);
     eventBus.emit('test-event');
 
-    expect(text).to.be.a('string').and.to.equal('mock');
-  });
-
-  it('off event', () => {
-    eventBus.off('test-event', mock);
-    eventBus.emit('test-event');
-
-    expect(text).to.be.a('null');
+    expect(mock).toHaveBeenCalled();
   });
 });
